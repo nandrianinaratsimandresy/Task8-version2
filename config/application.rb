c9752a51ed4a9307c91d898533f6266b074a9389
Nandrianina <nandrianinaratsimandresy23@gmail.com>
2020-11-15 11:59:43 +0000

    Fixed config application problem

--- config/application.rb
+++ config/application.rb
@@ -1,2 +1,13 @@
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module Sample
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+  end
+end
